cd031eded8eeaf32beea7e2830ddc4b06e3a268c
Mars Xue <marsxue0221@gmail.com>
2019-04-09 13:06:17 +1000

    Modify space view

--- GoatAST.hs
+++ GoatAST.hs
@@ -7,63 +7,63 @@ module GoatAST where
 type Ident = String
 
 data BaseType
-    = BoolType | IntType | FloatType
-    deriving (Show, Eq)
+  = BoolType | IntType | FloatType
+  deriving (Show, Eq)
 
 data LValue
-    = LId Ident
-    | LId Ident [Expr]
-    | LId Ident [Expr, Expr]
-    deriving (Show, Eq)
+  = LId Ident
+  | LId Ident [Expr]
+  | LId Ident [Expr, Expr]
+  deriving (Show, Eq)
 
 data Binop
-    = Op_add | Op_minus | Op_mul | Op_div | Op_or | Op_and | Op_equal | 
-    deriving (Show, Eq)
+  = Op_add | Op_minus | Op_mul | Op_div | Op_or | Op_and | Op_equal | 
+  deriving (Show, Eq)
 
 data Unop
-    = Op_neg | Op_minus
-    deriving (Show, Eq)
+  = Op_neg | Op_minus
+  deriving (Show, Eq)
 
 data Expr
-    = Id Ident
-    | Id Ident [Expr]
-    | Id Ident [Expr, Expr]
-    | BoolConst Bool
-    | IntConst Int
-    | FloatConst Float
-    | StrConst String
-    | (Expr)
-    | Add Expr Expr
-    | Minus Expr Expr
-    | Mul Expr Expr
-    | Div Expr Expr
-    | Or  Expr Expr
-    | And Expr Expr
-    | Equal Expr Expr
-    | NotEqual Expr Expr
-    | Less Expr Expr
-    | LessEqual Expr Expr
-    | Greater Expr Expr
-    | GreaterEqual Expr Expr
-    -- Unary
-    | UnaryNeg Expr
-    | UnaryMinus Expr
-    deriving (Show, Eq)
+  = Id Ident
+  | Id Ident [Expr]
+  | Id Ident [Expr, Expr]
+  | BoolConst Bool
+  | IntConst Int
+  | FloatConst Float
+  | StrConst String
+  | (Expr)
+  | Add Expr Expr
+  | Minus Expr Expr
+  | Mul Expr Expr
+  | Div Expr Expr
+  | Or  Expr Expr
+  | And Expr Expr
+  | Equal Expr Expr
+  | NotEqual Expr Expr
+  | Less Expr Expr
+  | LessEqual Expr Expr
+  | Greater Expr Expr
+  | GreaterEqual Expr Expr
+  -- Unary
+  | UnaryNeg Expr
+  | UnaryMinus Expr
+  deriving (Show, Eq)
 
 data Decl
-    = Decl Ident BaseType
-    deriving (Show, Eq)
+  = Decl Ident BaseType
+  deriving (Show, Eq)
 
 data Stmt
-    = Assign LValue Expr
-    | Read LValue
-    | Write Expr
-    | Call Ident [Expr]
-    | If Expr Then [Stmt] Fi
-    | IfElse Expr [Stmt] [Stmt]
-    | While Expr [Stmt]
-    deriving (Show, Eq)
+  = Assign LValue Expr
+  | Read LValue
+  | Write Expr
+  | Call Ident [Expr]
+  | If Expr Then [Stmt] Fi
+  | IfElse Expr [Stmt] [Stmt]
+  | While Expr [Stmt]
+  deriving (Show, Eq)
 
 data GoatProgram
-= Program [Decl] [Stmt]
-    deriving (Show, Eq)
+  = Program [Decl] [Stmt]
+  deriving (Show, Eq)
