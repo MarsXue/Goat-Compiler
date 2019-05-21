ef777a55ade175cdc24d17bdcd9363735b8eaa3a
Shijie Liu <shijiel2@student.unimelb.edu.au>
2019-05-21 13:41:30 +1000

    Update GoatCompiler.hs

--- GoatCompiler.hs
+++ GoatCompiler.hs
@@ -1,30 +1,67 @@
 import Control.Monad.State  
-import qualified Data.Map as Map  
+import Data.Map (
+    Map,
+    (!),
+    )
+import qualified Data.Map as Map
+import GoatAST
 
 data SymTable = SymTable 
     { labelCounter :: Int
     , slotCounter  :: Int
     , regCounter   :: Int
-    , code         :: String
-    , variables    :: Map String (BaseType, Int)
-    , procedure    :: Map String ([BaseType, Int])
+    -- , code         :: String
+    -- , variables    :: Map String (Decl, Int)
+    , procedures   :: Map String ([BaseType])
     } deriving (Show)
 
 
 
 nextAvaliableSlot :: State SymTable Int
-nextAvaliableSlot = do
-    symtable <- get
-    let slot = slotCounter symtable
-    put $ symtable {slotCounter = (slot + 1)}
-    return slot
+nextAvaliableSlot 
+    = do
+        st <- get
+        let slot = slotCounter st
+        put $ st {slotCounter = (slot + 1)}
+        return slot
 
 
-compile
-            
+compileProg :: GoatProg -> State SymTable ()
+compileProg (Prog ps)
+    = do
+        putProcedures ps
+        checkMain
+
+
+checkMain :: State SymTable ()
+checkMain
+    = do
+        st <- get
+        if Map.member "main" (procedures st)
+            then return ()
+            else error $ "no main procedure "
+
+
+putProcedures :: [Proc] -> State SymTable ()
+putProcedures [] = return ()
+putProcedures (p:ps)
+    = do
+        putProcedure p
+        putProcedures ps
+
+putProcedure :: Proc -> State SymTable ()
+putProcedure (Proc ident params _ _) 
+    = do
+        st <- get
+        let types = map (\(Param _ bt _) -> bt) params
+        if Map.member ident (procedures st)
+            then error $ "duplicated procedure " ++ ident
+            else put $ st { procedures = Map.insert ident types (procedures st) }
+        return ()
 
 
 
 
-test :: (Int, SymTable)
-test = runState nextAvaliableSlot (SymTable {labelCounter = 0, slotCounter = 0, regCounter = 0})
+
+test :: ((), SymTable)
+test = runState (compileProg (Prog [Proc "g" [Param Val FloatType "x",Param Ref IntType "k"] [Decl IntType (DBaseVar "n"),Decl FloatType (DBaseVar "y"),Decl BoolType (ShapeVar "a" (SArray 8))] [Assign (IndexVar "a" (IArray (IntConst 7))) (BoolConst True),Assign (SBaseVar "k") (IntConst 42)],Proc "p" [Param Ref IntType "i"] [] [Assign (SBaseVar "i") (Minus (Add (Mul (IntConst 6) (Id (SBaseVar "i"))) (IntConst 4)) (Id (SBaseVar "i")))],Proc "main" [] [Decl IntType (DBaseVar "m"),Decl IntType (DBaseVar "n")] [Read (SBaseVar "n"),While (Greater (Id (SBaseVar "n")) (IntConst 1)) [Assign (SBaseVar "m") (Id (SBaseVar "n")),While (Greater (Id (SBaseVar "m")) (IntConst 0)) [If (Greater (Id (SBaseVar "m")) (IntConst 0)) [Assign (SBaseVar "n") (Minus (Id (SBaseVar "n")) (IntConst 1)),Assign (SBaseVar "m") (Minus (Id (SBaseVar "m")) (IntConst 1)),If (Equal (Id (SBaseVar "m")) (IntConst 0)) [Call "p" [Id (SBaseVar "n")]] []] [Assign (SBaseVar "m") (Minus (Id (SBaseVar "n")) (Id (SBaseVar "m"))),Assign (SBaseVar "m") (Minus (Id (SBaseVar "m")) (IntConst 1))]]]]])) (SymTable { labelCounter = 0, slotCounter = 0, regCounter = 0, procedures = Map.empty })
